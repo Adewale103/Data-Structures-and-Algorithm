77a7e764a89c80aef1ff25a405349c36705101da
Adewale Adeyinka <adeyinkawale13@gmail.com>
2022-10-26 17:31:23 +0100

    "added generate unique string task"

--- src/practice/GenerateUniqueStrings.java
+++ src/practice/GenerateUniqueStrings.java
@@ -1,7 +1,60 @@
 package practice;
 
-public class GenerateUniqueStrings {
-    public static void main(String[] args) {
+import java.util.HashSet;
+import java.util.Set;
 
+public class GenerateUniqueStrings {
+//    private static Set<String> cache = new HashSet<>();
+//    public static void main(String[] args) {
+//        generatePermutation("funmii",0,6);
+//    }
+//    public static String swapString(String a, int i, int j) {
+//        char[] b =a.toCharArray();
+//        char ch;
+//        ch = b[i];
+//        b[i] = b[j];
+//        b[j] = ch;
+//        return String.valueOf(b);
+//    }
+//
+//    public static void generatePermutation(String str, int start, int end)
+//    {
+//
+//        //Prints the permutations
+//        if (start == end-1 && (!cache.contains(str))) {
+//                System.out.println(str);
+//                cache.add(str);
+//        }
+//        else
+//        {
+//            for (int i = start; i < end; i++)
+//            {
+//                //Swapping the string by fixing a character
+//                str = swapString(str,start,i);
+//                //Recursively calling function generatePermutation() for rest of the characters
+//                generatePermutation(str,start+1,end);
+//                //Backtracking and swapping the characters again.
+//                str = swapString(str,start,i);
+//            }
+//        }
+//    }
+static int count = 0;
+public static void main(String[] args) {
+
+    String[] elements = {"a", "b", "c"};
+    int maxLength = 5;
+    combineStringFromElements(elements, "", maxLength);
+    System.out.println(count);
+}
+
+    private static void combineStringFromElements(String[] elements, String currentString, int maxLength) {
+        if (currentString.length() == maxLength) {
+            count++;
+            System.out.println(currentString);
+            return;
+        }
+        for (String element : elements) {
+            combineStringFromElements(elements, currentString + element, maxLength);
+        }
     }
 }
---
+++ src/practice/GenerateUniqueStrings2.java
@@ -0,0 +1,57 @@
+package practice;
+
+import java.util.*;
+
+public class GenerateUniqueStrings2 {
+
+    public static List<String> getAllLists(String[] elements, int lengthOfList)
+    {
+        if(lengthOfList == 1) return Arrays.stream(elements).toList();
+        else {
+            List<String> allLists = new ArrayList<>();
+
+            List<String> allSublists = getAllLists(elements, lengthOfList-1);
+
+            for (String element : elements) {
+                for (String allSublist : allSublists) {
+                    //add the newly appended combination to the list
+                    allLists.add((element + allSublist));
+                }
+            }
+            return allLists;
+        }
+    }
+
+    public static void main(String[] args){
+        List<String> totalResult = new ArrayList<>();
+        String chars = "a,b,c,d,f,g,h,j";
+        String[] database = chars.split(",");
+        for (int i = 0; i < database.length; i++) {
+            if(database[i].length() != 1){
+                throw new RuntimeException("Invalid input, Separate each character by a comma");
+            }
+        }
+        int minimumLength = 2;
+        int maximumLength = 7;
+        int expectedNumberOfStrings = 150000;
+        int sum = 0;
+        if(minimumLength < 1){
+            throw  new RuntimeException("The minimum length can not be less than 1");
+        }
+        for (int i = minimumLength; i <= maximumLength ; i++) {
+            sum += (Math.pow(database.length, i));
+        }
+
+        System.out.println("Maximum number of strings that can be generated is: "+sum);
+        if(expectedNumberOfStrings > sum){
+            throw new RuntimeException("The requested number of strings is greater than the strings that can be generated. Only "+sum+" strings can be generated");
+        }
+        for(int i=minimumLength; i<=maximumLength; i++){
+            List<String> result = getAllLists(database, i);
+            totalResult.addAll(result);
+        }
+        for (int i = 0; i < expectedNumberOfStrings; i++) {
+            System.out.println(totalResult.get(i));
+        }
+    }
+}
