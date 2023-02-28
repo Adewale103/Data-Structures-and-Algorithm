f1fe764fa995d19718762640574e1c6acd899dfc
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-02-28 22:25:50 +0100

    "added new leetcode solution"

---
+++ src/leet_code_solutions/MajorityElement.java
@@ -0,0 +1,26 @@
+package leet_code_solutions;
+
+import java.util.Arrays;
+
+public class MajorityElement {
+    public class Solution {
+        public int majorityElement(int[] num) {
+            if(num.length==1){
+                return num[0];
+            }
+            Arrays.sort(num);
+            int prev=num[0];
+            int count=1;
+            for(int i=1; i<num.length; i++){
+                if(num[i] == prev){
+                    count++;
+                    if(count > num.length/2) return num[i];
+                }else{
+                    count=1;
+                    prev = num[i];
+                }
+            }
+            return 0;
+        }
+    }
+}
