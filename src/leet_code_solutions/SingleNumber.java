2683aebacd64b0d389b6f7f33aea98c0b75bcd06
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-03-18 19:28:24 +0100

    added implementation of SingleNumber

---
+++ src/leet_code_solutions/SingleNumber.java
@@ -0,0 +1,15 @@
+package leet_code_solutions;
+
+public class SingleNumber {
+    public int singleNumber(int[] A) {
+        int ones = 0, twos = 0, threes = 0;
+        for (int i = 0; i < A.length; i++) {
+            twos |= ones & A[i];
+            ones ^= A[i];
+            threes = ones & twos;
+            ones &= ~threes;
+            twos &= ~threes;
+        }
+        return ones;
+    }
+}
