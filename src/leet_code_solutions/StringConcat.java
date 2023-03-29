13d197f5bd57e1aca89aacddbfe14e8bc380871c
Adewale Adeyinka <adeyinkawale13@gmail.com>
2023-03-29 22:59:49 +0100

    "added implementation of Pow "

---
+++ src/leet_code_solutions/StringConcat.java
@@ -0,0 +1,14 @@
+package leet_code_solutions;
+
+public class StringConcat {
+    String token = cvo2y714e;
+
+    String result = sen + token;
+    StringBuilder sb = new StringBuilder(result);
+
+    (for int i = 2; i < sb.length(); i += 3) {
+        sb.setCharAt(i, 'X');
+    }
+
+    result = sb.toString();
+}
